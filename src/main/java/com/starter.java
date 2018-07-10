cc02cc4656bfabd03b9e216d8e29715d4f165d3a
zude <zude@seeapp.com>
2018-07-10 10:24:02 +0800

    第一次提交

---
+++ src/main/java/com/starter.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class starter {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(starter.class, args);
+
+	}
+
+}
